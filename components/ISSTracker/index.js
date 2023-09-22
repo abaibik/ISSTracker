5022afc9ae3cae912e8c4d7c417a2718ce17e7a4
Aleksandra <abaibik@gmail.com>
2023-09-22 12:10:38 +0200

    move fetcher above component

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -4,21 +4,21 @@ import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
 export default function ISSTracker() {
-  const fetcher = async (url) => {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-      const error = new Error("An error occurred while fetching the data.");
-
-      error.info = await res.json();
-      error.status = res.status;
-      throw error;
-    }
-
-    return res.json();
-  };
-
   const { data, isLoading, error, mutate } = useSWR(URL, fetcher, {
     refreshInterval: 5000,
   });
