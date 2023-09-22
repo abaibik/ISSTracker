ac4076f8bc89a4136bf5545c42f818fc499f442e
Aleksandra <abaibik@gmail.com>
2023-09-22 11:52:35 +0200

    fix fetcher

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -6,7 +6,7 @@ const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
   const fetcher = async (url) => {
-    const res = await fetch(URL);
+    const res = await fetch(url);
 
     if (!res.ok) {
       const error = new Error("An error occurred while fetching the data.");
